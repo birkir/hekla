4ebb1aba50688901f084f402884a59ab93bac4b7
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-07-02 14:52:38 -0400

    More AppCenter build script work

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,10 +1,24 @@
-echo "PWD: $PWD"
-echo "APPCENTER_SOURCE_DIRECTORY: $APPCENTER_SOURCE_DIRECTORY"
-echo "APPCENTER_ANDROID_VARIANT: $APPCENTER_ANDROID_VARIANT"
-echo "APPCENTER_XCODE_PROJECT: $APPCENTER_XCODE_PROJECT"
+echo "[Environment] Initializing..."
 
 if [ ! -z "$GOOGLE_SERVICES_JSON" ]; then
+  echo "[Environment] Creating google-services.json"
   echo $GOOGLE_SERVICES_JSON | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/android/app/google-services.json"
 else
-  echo "No GOOGLE_SERVICES_JSON in environment variables"
+  echo "[Environment] No GOOGLE_SERVICES_JSON in environment variables"
 fi
+
+if [ ! -z "$GOOGLE_SERVICES_PLIST" ]; then
+  echo "[Environment] Creating GoogleService-Info.plist"
+  echo $GOOGLE_SERVICES_PLIST | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/Hekla/GoogleService-Info.plist"
+else
+  echo "[Environment] No GOOGLE_SERVICES_JSON in environment variables"
+fi
+
+echo "[Environment] Creating .env file"
+for KEY in $(cat .env_example | egrep "^[A-Za-z]+" | sed 's/\"/\\\"/g' | sed -n 's|\(.*\)=\(.*\)|\1|p'); do
+  echo "$KEY=$(printf '%s\n' "${!KEY}")" >> "$APPCENTER_SOURCE_DIRECTORY/.env"
+done
+
+echo "[Environment] Done"
+
+source "$APPCENTER_SOURCE_DIRECTORY/scripts/build-env.sh"
