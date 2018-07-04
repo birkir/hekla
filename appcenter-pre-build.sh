10a711f34789641c1b77730fb132b01e1170d6d4
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-07-03 21:50:50 -0400

    AppCenter [skip ci]

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,47 +1,34 @@
-printenv
-
-brew cask install fastlane
-
-if [ ! -z "$APPCENTER_XCODE_PROJECT"]; then
-  cd ios; fastlane appcenter_release; cd -
-fi
-
-if [ ! -z "$APPCENTER_ANDROID_VARIANT" ]; then
-  cd android; fastlane appcenter_release; cd -
-fi
-
-
-echo "[Environment] Initializing..."
-
 if [ ! -z "$GOOGLE_SERVICES_JSON" ]; then
-  echo "[Environment] Creating google-services.json"
   echo $GOOGLE_SERVICES_JSON | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/android/app/google-services.json"
-else
-  echo "[Environment] No GOOGLE_SERVICES_JSON in environment variables"
 fi
 
 if [ ! -z "$GOOGLE_SERVICES_PLIST" ]; then
-  echo "[Environment] Creating GoogleService-Info.plist"
   echo $GOOGLE_SERVICES_PLIST | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/Hekla/GoogleService-Info.plist"
-else
-  echo "[Environment] No GOOGLE_SERVICES_PLIST in environment variables"
 fi
 
-echo "[Environment] Creating .env file"
 for KEY in $(cat .env_example | egrep "^[A-Za-z]+" | sed 's/\"/\\\"/g' | sed -n 's|\(.*\)=\(.*\)|\1|p'); do
   echo "$KEY=$(printf '%s\n' "${!KEY}")" >> "$APPCENTER_SOURCE_DIRECTORY/.env"
 done
 
-echo "[Environment] Done"
-
 source "$APPCENTER_SOURCE_DIRECTORY/scripts/build-env.sh"
 
-brew cask install fastlane
+COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 
-if [ ! -z "$APPCENTER_XCODE_PROJECT"]; then
-  cd ios; fastlane appcenter_release; cd -
-fi
+if [[ $COMMIT_MESSAGE != *"[BUILD]"* ]]; then
 
-if [ ! -z "$APPCENTER_ANDROID_VARIANT" ]; then
-  cd android; fastlane appcenter_release; cd -
+  npm install -g code-push
+  code-push login --accessKey $CODEPUSH_ACCESS_KEY
+
+  if [ -z "$APPCENTER_XCODE_PROJECT"]; then
+    code-push release-react $IOS_CODEPUSH_APPID ios --outputDir ./build --plistFile ./ios/Hekla/Info.plist --description "$COMMIT_MESSAGE"
+    sentry-cli react-native codepush $IOS_CODEPUSH_APPID ios ./build --bundle-id $IOS_BUNDLE_ID
+    APPCENTER_APPID="$IOS_CODEPUSH_APPID"
+  fi
+  if [ -z "$APPCENTER_ANDROID_VARIANT" ]; then
+    code-push release-react $ANDROID_CODEPUSH_APPID android --outputDir build --description "$COMMIT_MESSAGE"
+    sentry-cli react-native codepush $ANDROID_CODEPUSH_APPID android ./build --bundle-id $ANDROID_BUNDLE_ID
+    APPCENTER_APPID="$ANDROID_CODEPUSH_APPID"
+  fi
+
+  curl -X PATCH "https://api.appcenter.ms/v0.1/apps/$APPCENTER_APPID/builds/$APPCENTER_BUILD_ID" -H "accept: application/json" -H "X-API-Token: $APPCENTER_API_KEY" -H "Content-Type: application/json" -d "{ \"status\": \"cancelling\" }"
 fi
