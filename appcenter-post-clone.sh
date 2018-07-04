0e92732bac1007be23fca22fbfd0448e225256f3
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-07-03 22:58:24 -0400

    Testing commit 8

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -13,13 +13,15 @@ COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 
 echo "hello" > hello.txt
 
+
+
 echo $ID_RSA | base64 --decode > id_rsa
 chmod 400 id_rsa
 eval `ssh-agent -s`
 ssh-add id_rsa
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
-git remote set-url --push origin git@github.com/birkir/hekla.git
+git remote set-url --push origin git@github.com:birkir/hekla.git
 git ls-remote
 
 git checkout master
