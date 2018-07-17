c0fb2fa078cd58357ef5d61b57f9142fa9e86832
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-07-17 15:18:48 -0400

    Fix almost age

--- src/utils/age.ts
+++ src/utils/age.ts
@@ -1,7 +1,7 @@
 import distance_in_words_to_now from 'date-fns/distance_in_words_to_now';
 
 export default (date: Date) => distance_in_words_to_now(date)
-  .replace(/about|ago|over/, '')
+  .replace(/almost|about|ago|over/, '')
   .replace('less than a minute', '<1m')
   .replace(/months?/, 'mo')
   .replace(/(year|week|day|hour|minute|second)s?/, r => r[0])
